8591aac170e02c10d8e525096c8e8889b01b98ac
AliaksanderPush <tribeca1983@gmail.com>
2022-05-20 15:00:28 +0300

    tab

--- App.tsx
+++ App.tsx
@@ -1,73 +1,108 @@
 import { SignUp } from './screens/SingUp/SingUp';
 import { SignIn } from './screens/SingIn/SingIn';
-import { Home } from './screens/Home';
+import { Chat } from './screens/Chat/Chat';
 import { Feed } from './screens/Feed/Feed';
-import { NavigationContainer, NavigatorScreenParams } from '@react-navigation/native';
+import { Likes } from './screens/Likes/Likes';
+import { Friends } from './screens/Friends/Friends';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Account } from './screens/Account/Account';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { AntDesign } from '@expo/vector-icons';
+import { Octicons } from '@expo/vector-icons';
+import React from 'react';
 
 export type RootStackParams = {
 	SignUp: undefined;
 	SignIn: undefined;
-	Account:NavigatorScreenParams<ButtomStackParams>;
-	
+	TabScreenStack: undefined;
 };
 
 export type ButtomStackParams = {
-	Home:undefined
-	Feed:undefined
+	Chat: undefined;
+	Feed: undefined;
+	Account: undefined;
+};
 
-}
-
+export type DrawerStackParams = {
+	Friends: undefined;
+	Likes: undefined;
+};
 
 const Stack = createNativeStackNavigator<RootStackParams>();
+const ButtomStack = createBottomTabNavigator<ButtomStackParams>();
+const Drawer = createDrawerNavigator();
 
 export const AuthScreenStack = () => {
 	return (
-	  <Stack.Navigator initialRouteName='SignIn' screenOptions={{
-		headerShown: false,
-	  }}>
-		<Stack.Screen
-		  name="SignIn"
-		  component={SignIn}
-		/>
-		<Stack.Screen name="SignUp" component={SignUp} />
-		<Stack.Screen name="Account" component={ButtomStack} />
-	  </Stack.Navigator>
+		<Stack.Navigator
+			initialRouteName='SignIn'
+			screenOptions={{
+				headerShown: false,
+			}}
+		>
+			<Stack.Screen name='SignIn' component={SignIn} />
+			<Stack.Screen name='SignUp' component={SignUp} />
+			<Stack.Screen name='TabScreenStack' component={TabScreenStack} />
+		</Stack.Navigator>
 	);
-  };
+};
 
-  export const ButtomStack = createBottomTabNavigator<ButtomStackParams>()
-  const TabScreenStack = () => {
-    return (
-       
-	    <ButtomStack.Navigator  screenOptions={{ headerShown: false }}>
-		   <ButtomStack.Screen name='Home' component={Home} />
-	    	<ButtomStack.Screen name='Feed' component={Feed} />
-     	</ButtomStack.Navigator>
-		 
-	)
-  }
-
-
-
-
+const TabScreenStack = () => {
+	return (
+		<ButtomStack.Navigator
+			screenOptions={{
+				tabBarActiveTintColor: '#2BB24C',
+				tabBarInactiveTintColor: '#9B9B9B',
+				headerShown: false,
+			}}
+		>
+			<ButtomStack.Screen
+				name='Account'
+				component={Account}
+				options={{
+					title: 'Profile',
+					tabBarIcon: ({ focused, size, color }) => (
+						<Octicons name='feed-person' size={32} color={color} />
+					),
+				}}
+			/>
+			<ButtomStack.Screen
+				name='Chat'
+				component={Chat}
+				options={{
+					tabBarIcon: ({ focused, size, color }) => (
+						<AntDesign name='wechat' size={32} color={color} />
+					),
+				}}
+			/>
+			<ButtomStack.Screen
+				name='Feed'
+				component={Feed}
+				options={{
+					tabBarIcon: ({ focused, size, color }) => (
+						<Octicons name='feed-heart' size={32} color={color} />
+					),
+				}}
+			/>
+		</ButtomStack.Navigator>
+	);
+};
 
 const App = () => {
 	return (
 		<Provider store={store}>
-		 <SafeAreaProvider>
-		 <NavigationContainer>
-          <AuthScreenStack/ >
-		 </NavigationContainer>
-		 </SafeAreaProvider>
+			<SafeAreaProvider>
+				<NavigationContainer>
+					<AuthScreenStack />
+				</NavigationContainer>
+			</SafeAreaProvider>
 		</Provider>
 	);
-}
+};
 
 export default App;
